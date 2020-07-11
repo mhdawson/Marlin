ae7fe8fc74090266d9b2c992220d7445b61c30e5
Michael Dawson <mdawson@devrus.com>
2020-07-11 15:55:31 -0400

    adjust bed size for extruder config 1

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1109,8 +1109,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 450
-#define Y_BED_SIZE 450
+#define X_BED_SIZE 400
+#define Y_BED_SIZE 410
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
