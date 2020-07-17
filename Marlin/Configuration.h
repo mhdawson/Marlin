61c8b3de304c733682f1da8328ea2d24009f45fa
Michael Dawson <mdawson@devrus.com>
2020-07-17 10:59:04 -0400

    adjust bed size for current hot bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1110,7 +1110,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 400
-#define Y_BED_SIZE 410
+#define Y_BED_SIZE 295
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
